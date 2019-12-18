c04b26e58c5f6b1743389208acd09852e5462d94
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-12-17 16:28:46 -0800

    setup of legacy codebase

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env = process.env.NODE_ENV;
-const mongoUri = `mongodb://database/graph`;
+const mongoUri = `mongodb://localhost/graph`;
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
