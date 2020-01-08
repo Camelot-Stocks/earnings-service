d1934730250a6ec1f2809ff59a4819b8ea3a23df
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-01-08 15:49:29 -0800

    modifications for use on AWS EC2 instance

--- server/db/cassandra/index.js
+++ server/db/cassandra/index.js
@@ -1,13 +1,13 @@
 const cassandra = require('cassandra-driver');
 const log = require('fancy-log');
 
-const localDatacenter = 'datacenter1';
+const localDatacenter = 'Test Cluster';
 const loadBalancingPolicy = new cassandra.policies.loadBalancing.DCAwareRoundRobinPolicy(localDatacenter); 
 const clientOptions = {
    policies : {
       loadBalancing : loadBalancingPolicy
    },
-   contactPoints: ['127.0.0.1']
+   contactPoints: ['172.31.17.118']
 }; 
 
 const client = new cassandra.Client(clientOptions);
